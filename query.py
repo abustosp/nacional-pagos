5446c6ba04252c5c4eeb195e8ea7881a0a71b141
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2024-06-18 12:25:32 -0300

    chore: Refactor query.py to include query functions for  cobros

--- query.py
+++ query.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.engine import Connection
 import pandas as pd
 
+
 def listador_de_databases(ip:str , user:str , password:str):
     """
     Listar las bases de datos de un servidor MySQL
@@ -31,8 +32,8 @@ def listador_de_databases(ip:str , user:str , password:str):
     for i in borrar:
         databases.remove(i)
 
-
     return databases
+
 
 def get_connection(ip:str , database:str , user:str , password:str):
     """
@@ -54,9 +55,10 @@ def get_connection(ip:str , database:str , user:str , password:str):
     
     return connection
 
+
 def query_pago(connection:Connection, desde:str, hasta:str):
     """
-    Realizar una consulta a la base de datos
+    Realizar una consulta a la base de datos de pagos
 
     Args:
         connection (Connection): conexión a la base de datos
@@ -70,16 +72,43 @@ def query_pago(connection:Connection, desde:str, hasta:str):
     FROM cmp_pago_medio
     LEFT JOIN cmp_pago ON cmp_pago_medio.pago = cmp_pago.numero 
     LEFT JOIN iva_persona ON cmp_pago.proveedor = iva_persona.id
-    where cmp_pago.fecha BETWEEN :desde AND :hasta;""")
+    where cmp_pago.fecha BETWEEN :desde AND :hasta
+    ;""")
 
     # Realizar consulta y guardar en un DataFrame
     tabla = pd.read_sql_query(query , connection, params={'desde': desde, 'hasta': hasta})
 
     return tabla
 
+
+def query_cobro(connection:Connection, desde:str, hasta:str):
+    """
+    Realizar una consulta a la base de datos de cobros
+
+    Args:
+        connection (Connection): conexión a la base de datos
+        desde (str): fecha de inicio de la consulta
+        hasta (str): fecha de fin de la consulta
+    """
+    
+    # crear query de cobros
+    query = text(f"""
+    SELECT vta_cobro.*, vta_cobro_medio.*, iva_persona.razon_social
+    FROM vta_cobro
+    LEFT JOIN vta_cobro_medio ON vta_cobro.numero = vta_cobro_medio.cliente
+    LEFT JOIN iva_persona ON vta_cobro.cliente = iva_persona.id
+    where vta_cobro.fecha BETWEEN :desde AND :hasta
+    ;""")
+    
+    # Realizar consulta y guardar en un DataFrame
+    tabla = pd.read_sql_query(query , connection, params={'desde': desde, 'hasta': hasta})
+    
+    return tabla
+
+
 def query_IVA(connection:Connection, desde:str, hasta:str):
     """
-    Realizar una consulta a la base de datos
+    Realizar una consulta a la base de datos de comprobantes de IVA
 
     Args:
         connection (Connection): conexión a la base de datos
@@ -92,7 +121,8 @@ def query_IVA(connection:Connection, desde:str, hasta:str):
     SELECT iva_comprobante.modulo, iva_comprobante.tipo, comprobante, persona, iva_persona.razon_social, fecha, periodo , total
     FROM iva_comprobante
     LEFT JOIN iva_persona ON iva_comprobante.persona = iva_persona.id
-    where fecha BETWEEN :desde AND :hasta;""")
+    where fecha BETWEEN :desde AND :hasta
+    ;""")
 
     # Realizar consulta y guardar en un DataFrame
     tabla = pd.read_sql_query(query , connection, params={'desde': desde, 'hasta': hasta})
@@ -100,6 +130,7 @@ def query_IVA(connection:Connection, desde:str, hasta:str):
     # multiplicar por -1 los valores de la columna total si el tipo es 'NC' 'NCE'
     #tabla.loc[tabla['tipo'].str.contains('NC|NCE'), 'total'] = tabla.loc[tabla['tipo'].str.contains('NC|NCE'), 'total'] * -1
     tabla.loc[tabla['tipo'].isin(['NC', 'NCE']), 'total'] = tabla.loc[tabla['tipo'].isin(['NC', 'NCE']), 'total'] * -1
+    
     return tabla
 
 
@@ -121,6 +152,7 @@ def obtener_pagos_masivos(ip:str , user:str , password:str , desde:str , hasta:s
             conección = get_connection(ip , i , user , password)
             pago = query_pago(conección, desde, hasta)
             iva = query_IVA(conección, desde, hasta)
+            cobro = query_cobro(conección, desde, hasta)
             
             # realizar tablas dinámicas
             
@@ -134,8 +166,9 @@ def obtener_pagos_masivos(ip:str , user:str , password:str , desde:str , hasta:s
             
             # Exportar a un mismo excel
             with pd.ExcelWriter(f'resultados/datos - {i} - {desde} - {hasta}.xlsx') as writer:
-                pago.to_excel(writer, sheet_name='Pagos', index=False)
                 iva.to_excel(writer, sheet_name='IVA', index=False)
+                pago.to_excel(writer, sheet_name='Pagos', index=False)
+                cobro.to_excel(writer, sheet_name='Cobros', index=False)
                 iva_td_periodo.to_excel(writer, sheet_name='IVA_TD_periodo', index=False)
                 iva_td_resumen.to_excel(writer, sheet_name='IVA_TD_resumen', index=False)
             print(f'Base de datos {i} exportada correctamente')
